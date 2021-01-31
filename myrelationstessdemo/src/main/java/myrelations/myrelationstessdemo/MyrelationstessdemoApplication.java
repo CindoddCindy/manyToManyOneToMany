10d3f2a25fde5dbcede7e3613142dfe25a9dec5b
CindoddCindy <cindodcindy@gmail.com>
2021-01-31 07:29:10 +0700

    tambah package

---
+++ myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/MyrelationstessdemoApplication.java
@@ -0,0 +1,13 @@
+package myrelations.myrelationstessdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrelationstessdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyrelationstessdemoApplication.class, args);
+	}
+
+}
