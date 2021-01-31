021c11f0293c9a94c761055cafff9747b956b56f
CindoddCindy <cindodcindy@gmail.com>
2021-01-31 18:21:43 +0700

    baru bkin mapping di repository

--- myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/repository/UserRepository.java
+++ myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package myrelations.myrelationstessdemo.repository;
 
 import myrelations.myrelationstessdemo.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +16,15 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>
 {
+
+    Page<User> findByLibraryId(Long libraryId, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Book b WHERE b.library.id = ?1")
+    void deleteByLibraryId(Long libraryId);
+
+
 
     Optional<User> findByEmail(String email);
     //@Query(value = "delete  from t_user_roles where users_id= :user_id", nativeQuery = true)
