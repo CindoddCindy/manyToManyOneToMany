e6bcdef2cecb9cdded7189860bb9e13b63f3ef61
CindoddCindy <cindodcindy@gmail.com>
2021-01-31 17:55:44 +0700

    nambah rpeository

---
+++ myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package myrelations.myrelationstessdemo.repository;
+
+import myrelations.myrelationstessdemo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>
+{
+
+    Optional<User> findByEmail(String email);
+    //@Query(value = "delete  from t_user_roles where users_id= :user_id", nativeQuery = true)
+    //void deleteRelation(@Param("user_id") Long user_id);
+    //@Query("from User where id= :id")
+    //User  findByUserId(@Param("id") Long id);
+    @Query(value = "select * from t_user", nativeQuery = true)
+    List<User> findAllByUserId();
+}
