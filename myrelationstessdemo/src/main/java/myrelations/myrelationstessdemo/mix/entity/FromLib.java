95d689e13ca6fc3051c07e5d4c4d7b260d616864
CindoddCindy <cindodcindy@gmail.com>
2021-01-31 08:42:43 +0700

    tambah repository

--- myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/mix/entity/FromLib.java
+++ myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/mix/entity/FromLib.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
-@Table(name = "fromLib")
+@Table(name = "from_lib")
 public class FromLib {
 
 
--- myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/mix/repository/FromLibRepository.java
+++ myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/mix/repository/FromLibRepository.java
@@ -1,5 +1,6 @@
 package myrelations.myrelationstessdemo.mix.repository;
 
+import myrelations.myrelationstessdemo.manytomany.entitiy.User;
 import myrelations.myrelationstessdemo.mix.entity.FromLib;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,10 +10,21 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface FromLibRepository extends JpaRepository<FromLib, Integer> {
 
     Page<FromLib> findByLibraryId(Integer libraryId, Pageable pageable);
+
+    Optional<FromLib> findByEmail(String email);
+    //@Query(value = "delete  from t_user_roles where users_id= :user_id", nativeQuery = true)
+    //void deleteRelation(@Param("user_id") Long user_id);
+    //@Query("from User where id= :id")
+    //User  findByUserId(@Param("id") Long id);
+    @Query(value = "select * from from_lib", nativeQuery = true)
+    List<FromLib> findAllByFromLibId();
 
 
 
