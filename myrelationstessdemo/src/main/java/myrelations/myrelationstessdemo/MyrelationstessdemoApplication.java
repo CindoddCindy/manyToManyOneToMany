802dad3ee5bbfe75d23f56f5c69dccaf95b38fa3
CindoddCindy <cindodcindy@gmail.com>
2021-02-03 19:49:26 +0700

    ganti lagi

--- myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/MyrelationstessdemoApplication.java
+++ myrelationstessdemo/src/main/java/myrelations/myrelationstessdemo/MyrelationstessdemoApplication.java
@@ -2,8 +2,10 @@ package myrelations.myrelationstessdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MyrelationstessdemoApplication {
 
 	public static void main(String[] args) {
